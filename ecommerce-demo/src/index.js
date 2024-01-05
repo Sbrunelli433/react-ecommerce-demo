388b4523340a6d110a6ff46eefddce2e03e9cdca
Steve Brunelli <brunes46@gmail.com>
2024-01-05 13:32:06 -0600

    configuration and setup for redux

--- ecommerce-demo/src/index.js
+++ ecommerce-demo/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -8,19 +9,22 @@ import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
 
 import "./index.scss";
+import { store } from "./store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <CartProvider>
-        <UserProvider>
-          <CategoriesProvider>
-            <App />
-          </CategoriesProvider>
-        </UserProvider>
-      </CartProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <CartProvider>
+          <UserProvider>
+            <CategoriesProvider>
+              <App />
+            </CategoriesProvider>
+          </UserProvider>
+        </CartProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
---
+++ ecommerce-demo/src/store/store.js
@@ -0,0 +1,11 @@
+import { compose, configureStore , applyMiddleware } from "redux";
+import logger from "redux-logger";
+import { rootReducer } from "./root-reducer";
+
+const middleWares = [logger];
+
+const composedEnhancers = compose(applyMiddleware(...middleWares));
+
+//root-reducer
+
+export const store = configureStore(rootReducer, undefined, composedEnhancers);
